554b087e7c16c99534660804789a758845d7b785
Lea NAGY <lnagy@e2r12p20.42.fr>
2018-05-28 18:16:39 +0200

    trying_to_datafuckingtables

---
+++ d04/ex01/NewCheatSheet/app/controllers/welcome_controller.rb
@@ -0,0 +1,32 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  # def convention
+  # end
+  # def console
+  # end
+  # def ruby
+  # end
+  # def ruby_concept
+  # end
+  # def ruby_numbers
+  # end
+  # def ruby_strings
+  # end
+  # def ruby_arrays
+  # end
+  # def ruby_hashes
+  # end
+  # def rails
+  # end
+  # def rails_folder_structure
+  # end
+  # def rails_commands
+  # end
+  # def rails_erb
+  # end
+  # def editor
+  # end
+  # def help
+  # end
+end
---
+++ d04/ex02/CheatSheet/config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+
+  root 'welcome#convention'
+  # get 'welcome/index'
+  get '/console', to: 'welcome#console'
+  get '/convention', to: 'welcome#convention'
+  get '/editor', to: 'welcome#editor'
+  get '/help', to: 'welcome#help'
+  get '/rails', to: 'welcome#rails'
+  get '/ruby', to: 'welcome#ruby'
+  get '/rails-erb', to: 'welcome#rails_erb'
+  get '/rails-commands', to: 'welcome#rails_commands'
+  get '/rails-folder-structure', to: 'welcome#rails_folder_structure'
+  get '/ruby-arrays', to: 'welcome#ruby_arrays'
+  get '/ruby-concepts', to: 'welcome#ruby_concept'
+  get '/ruby-strings', to: 'welcome#ruby_strings'
+  get '/ruby-numbers', to: 'welcome#ruby_numbers'
+  get '/ruby-hashes', to: 'welcome#ruby_hashes'
+  get '/quicksearch', to: 'welcome#quick_search'
+
+
+  # For deta111ils on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
