17e473c3a12cd5aaba71cd45ba9a2af16cb7faf6
Lea NAGY <lnagy@e1r3p4.42.fr>
2018-05-30 19:02:12 +0200

    title + map+

---
+++ rush00/Moviemon/app/controllers/moviemon_controller.rb
@@ -0,0 +1,16 @@
+# $view
+# $selected
+# $game
+# $player = strengh, position
+
+
+class MoviemonController < ApplicationController
+
+puts 'cic'
+
+end
+
+# class Moviemon
+#   strengh = 40
+#   director =
+# end
